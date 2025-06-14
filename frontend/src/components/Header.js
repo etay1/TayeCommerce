2705a0c58bc46a57ad8fa8d4a51a97c8a24fd783
Elijah Tay <elijahtay911@gmail.com>
2025-06-13 23:34:31 -0400

    [ADD] components: wip Header for early testing

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,13 +1,46 @@
-import React from 'react'
+import Container from '../../node_modules/react-bootstrap/Container';
+import Nav from '../../node_modules/react-bootstrap/Nav';
+import Navbar from '../../node_modules/react-bootstrap/Navbar';
+import NavDropdown from '../../node_modules/react-bootstrap/NavDropdown';
+import Form from '../../node_modules/react-bootstrap/Form';
+import Button from '../../node_modules/react-bootstrap/Button';
 
 function Header() {
   return (
-    <div>
-        <header>
-            Header
-        </header>
-    </div>
-  )
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">TayeCommerce</Navbar.Brand>
+        <Form className="d-flex me-auto">
+          <Form.Control
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+          />
+          <Button type="submit">Submit</Button>
+        </Form>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#cart">Cart</Nav.Link>
+            <Nav.Link href="#signup">Sign In</Nav.Link>
+            <NavDropdown title="Profile" id="collapsible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default Header
+export default Header;
+
