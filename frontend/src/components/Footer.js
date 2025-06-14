35d02793bf2fc74a39499c5347ae2ffd6b47c2a9
Elijah Tay <elijahtay911@gmail.com>
2025-06-14 13:54:37 -0400

    [ADD] footer: copyright

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
 
 function Footer() {
   return (
     <div>
-        <footer>
-            Footer
-        </footer>
+      <footer>
+        <Container>
+          <Row>
+            <Col className='text-center py-3'>
+              <p>Copyright &copy; TayeCommerce</p>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
     </div>
   )
 }
