83a51159b142c4c69a48e58ea74aaf8c112582d2
Elijah Tay <elijahtay911@gmail.com>
2025-07-13 14:45:46 -0400

    [REF] components: temporary rework of products

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -7,7 +7,7 @@ function HomeScreen() {
     return (
         <div>
             <h1>Latest Products</h1>
-            <Row>
+            <Row className="g-4">
                 {products.map(product => (
                     <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                         <Product product={product} />
